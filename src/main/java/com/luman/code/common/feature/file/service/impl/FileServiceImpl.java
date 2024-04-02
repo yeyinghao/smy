75b5d40979ae0ea399fa115f3f725b42e647d6a3
yeyinghao <1113531030@qq.com>
2024-04-02 22:20:59 +0800

    重新抽取

--- src/main/java/com/luman/code/common/feature/file/service/impl/FileServiceImpl.java
+++ src/main/java/com/luman/code/common/feature/file/service/impl/FileServiceImpl.java
@@ -9,25 +9,21 @@ import cn.hutool.core.io.IoUtil;
 import cn.hutool.http.Header;
 import com.luman.code.common.enums.CommErrorEnum;
 import com.luman.code.common.exception.Assert;
+import com.luman.code.common.feature.file.config.MinioConfig;
 import com.luman.code.common.feature.file.service.FileService;
 import io.minio.*;
-import io.minio.errors.*;
 import io.minio.http.Method;
 import io.minio.messages.DeleteError;
 import io.minio.messages.DeleteObject;
 import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLEncoder;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 /**
@@ -41,21 +37,14 @@ import java.util.*;
 public class FileServiceImpl implements FileService {
 
 	/**
-	 * 文件代理
+	 * minio客户端
 	 */
 	private final MinioClient minioClient;
 
 	/**
-	 * 其他文件的最大大小
+	 * minio配置
 	 */
-	@Value("${minio.fileSize}")
-	private Long fileSize;
-
-	/**
-	 * bucket名称
-	 */
-	@Value("${minio.bucket-name}")
-	private String bucketName;
+	private final MinioConfig minioConfig;
 
 	@Override
 	@SneakyThrows
@@ -64,18 +53,25 @@ public class FileServiceImpl implements FileService {
 	}
 
 	@Override
-	@SneakyThrows
 	public void uploadFile(String objectName, String fileName, InputStream inputStream) {
 		putObject(objectName, fileName, inputStream);
 	}
 
-	private void putObject(String objectName, String fileName, InputStream inputStream) throws ErrorResponseException, InsufficientDataException, InternalException, InvalidKeyException, InvalidResponseException, IOException, NoSuchAlgorithmException, ServerException, XmlParserException {
+	/**
+	 * 推送对象
+	 *
+	 * @param objectName  对象名称
+	 * @param fileName    文件名称
+	 * @param inputStream 输入流
+	 */
+	@SneakyThrows
+	private void putObject(String objectName, String fileName, InputStream inputStream) {
 		try {
 			Assert.notBlank(fileName, CommErrorEnum.BIZ_PROCESS_FAIL, "文件名称不能为空");
 			// 下载文件时自动添加文件名
 			Map<String, String> headers = new HashMap<>();
 			headers.put(Header.CONTENT_DISPOSITION.getValue(), "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
-			PutObjectArgs putObjectArgs = PutObjectArgs.builder().bucket(bucketName).object(objectName).stream(inputStream, -1, fileSize).headers(headers).build();
+			PutObjectArgs putObjectArgs = PutObjectArgs.builder().bucket(minioConfig.getBucketName()).object(objectName).stream(inputStream, -1, minioConfig.getFileSize()).headers(headers).build();
 			minioClient.putObject(putObjectArgs);
 		} finally {
 			IoUtil.close(inputStream);
@@ -91,7 +87,7 @@ public class FileServiceImpl implements FileService {
 	@Override
 	@SneakyThrows
 	public String getObjectFileUrl(String objectName, Long expiry) {
-		return minioClient.getPresignedObjectUrl(GetPresignedObjectUrlArgs.builder().method(Method.GET).bucket(bucketName).object(objectName).expiry(expiry.intValue()).build());
+		return minioClient.getPresignedObjectUrl(GetPresignedObjectUrlArgs.builder().method(Method.GET).bucket(minioConfig.getBucketName()).object(objectName).expiry(expiry.intValue()).build());
 	}
 
 	@Override
@@ -99,7 +95,7 @@ public class FileServiceImpl implements FileService {
 	public byte[] downloadObject(String objectName) {
 		InputStream inputStream = null;
 		try {
-			inputStream = minioClient.getObject(GetObjectArgs.builder().bucket(bucketName).object(objectName).build());
+			inputStream = minioClient.getObject(GetObjectArgs.builder().bucket(minioConfig.getBucketName()).object(objectName).build());
 			return IoUtil.readBytes(inputStream);
 		} finally {
 			IoUtil.close(inputStream);
@@ -109,7 +105,7 @@ public class FileServiceImpl implements FileService {
 	@Override
 	@SneakyThrows
 	public StatObjectResponse statObject(String objectName) {
-		return minioClient.statObject(StatObjectArgs.builder().bucket(bucketName).object(objectName).build());
+		return minioClient.statObject(StatObjectArgs.builder().bucket(minioConfig.getBucketName()).object(objectName).build());
 	}
 
 	@Override
@@ -118,7 +114,7 @@ public class FileServiceImpl implements FileService {
 		for (String objectName : objectNameList) {
 			objects.add(new DeleteObject(objectName));
 		}
-		Iterable<Result<DeleteError>> results = minioClient.removeObjects(RemoveObjectsArgs.builder().bucket(bucketName).objects(objects).build());
+		Iterable<Result<DeleteError>> results = minioClient.removeObjects(RemoveObjectsArgs.builder().bucket(minioConfig.getBucketName()).objects(objects).build());
 		Assert.notEmpty(results, CommErrorEnum.BIZ_PROCESS_FAIL, "批量删除失败");
 	}
 
@@ -126,7 +122,7 @@ public class FileServiceImpl implements FileService {
 	@SneakyThrows
 	public List<String> listObjectNames() {
 		List<String> listObjectNames = new ArrayList<>();
-		Iterable<Result<Item>> myObjects = minioClient.listObjects(ListObjectsArgs.builder().bucket(bucketName).build());
+		Iterable<Result<Item>> myObjects = minioClient.listObjects(ListObjectsArgs.builder().bucket(minioConfig.getBucketName()).build());
 		for (Result<Item> result : myObjects) {
 			Item item = result.get();
 			listObjectNames.add(item.objectName());
