062ee7050f72b8fd50be5cca044900f0a87f5c53
yeyinghao <1113531030@qq.com>
2024-05-19 22:31:36 +0800

    重新抽取

--- smy-common/src/main/java/com/luman/smy/common/template/ExecuteTemplate.java
+++ smy-common/src/main/java/com/luman/smy/common/template/ExecuteTemplate.java
@@ -1,6 +1,13 @@
 package com.luman.smy.common.template;
 
+import cn.hutool.json.JSONUtil;
+import com.luman.smy.common.constant.CommConstant;
 import com.luman.smy.common.enums.BaseEnum;
+import com.luman.smy.common.enums.ErrorEnum;
+import com.luman.smy.common.exception.BizException;
+import com.luman.smy.common.util.LoggerUtil;
+import com.luman.smy.common.util.TimeUtil;
+import org.slf4j.Logger;
 
 import java.util.function.Supplier;
 
@@ -13,6 +20,13 @@ import java.util.function.Supplier;
 public interface ExecuteTemplate {
 
 	/**
+	 * 获取日志记录器
+	 *
+	 * @return {@link Logger }
+	 */
+	Logger getLogger();
+
+	/**
 	 * 执行
 	 *
 	 * @param baseEnum 基础枚举
@@ -20,7 +34,25 @@ public interface ExecuteTemplate {
 	 * @param objs     obj
 	 * @return {@link R}
 	 */
-	<R> R execute(BaseEnum baseEnum, Supplier<R> supplier, Object... objs);
+	default <R> R execute(BaseEnum baseEnum, Supplier<R> supplier, Object... objs) {
+		long startTime = System.currentTimeMillis();
+		String result = CommConstant.N;
+		ErrorEnum errorEnum = null;
+		Object[] subMsg = null;
+		R res = null;
+		try {
+			res = supplier.get();
+			result = CommConstant.Y;
+			return res;
+		} catch (BizException e) {
+			subMsg = e.getSubMessage();
+			errorEnum = e.getErrorEnum();
+			result = e.getResult();
+			throw e;
+		} finally {
+			LoggerUtil.info(getLogger(), baseEnum, JSONUtil.toJsonStr(res), result, errorEnum, subMsg, JSONUtil.toJsonStr(objs), TimeUtil.getCostTime(startTime));
+		}
+	}
 
 	/**
 	 * 执行
@@ -29,5 +61,21 @@ public interface ExecuteTemplate {
 	 * @param runnable 可运行
 	 * @param objs     obj
 	 */
-	void execute(BaseEnum baseEnum, Runnable runnable, Object... objs);
+	default void execute(BaseEnum baseEnum, Runnable runnable, Object... objs) {
+		long startTime = System.currentTimeMillis();
+		String result = CommConstant.N;
+		ErrorEnum errorEnum = null;
+		Object[] subMsg = null;
+		try {
+			runnable.run();
+			result = CommConstant.Y;
+		} catch (BizException e) {
+			subMsg = e.getSubMessage();
+			errorEnum = e.getErrorEnum();
+			result = e.getResult();
+			throw e;
+		} finally {
+			LoggerUtil.info(getLogger(), baseEnum, result, errorEnum, subMsg, JSONUtil.toJsonStr(objs), TimeUtil.getCostTime(startTime));
+		}
+	}
 }
--- smy-common/src/main/java/com/luman/smy/common/template/impl/RestExecuteTemplateImpl.java
+++ smy-common/src/main/java/com/luman/smy/common/template/impl/RestExecuteTemplateImpl.java
@@ -1,13 +1,10 @@
 package com.luman.smy.common.template.impl;
 
 import com.luman.smy.common.constant.LoggerConstant;
-import com.luman.smy.common.enums.BaseEnum;
 import com.luman.smy.common.template.ExecuteTemplate;
-import com.luman.smy.common.template.util.TemplateUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
-
-import java.util.function.Supplier;
 
 /**
  * 执行模板实现
@@ -20,12 +17,7 @@ import java.util.function.Supplier;
 public class RestExecuteTemplateImpl implements ExecuteTemplate {
 
 	@Override
-	public <R> R execute(BaseEnum baseEnum, Supplier<R> supplier, Object... objs) {
-		return TemplateUtil.execute(log, baseEnum, supplier, objs);
-	}
-
-	@Override
-	public void execute(BaseEnum baseEnum, Runnable runnable, Object... objs) {
-		TemplateUtil.execute(log, baseEnum, runnable, objs);
+	public Logger getLogger() {
+		return log;
 	}
 }
